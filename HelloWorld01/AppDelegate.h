1a6d541e4552871d96039084c8971fa6885ae673
git <git@qq.com>
2016-04-27 13:38:50 +0800

    Initial Commit

---
+++ HelloWorld01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld01
+//
+//  Created by shisheng on 16/4/27.
+//  Copyright (c) 2016年 shisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld01
+//
+//  Created by shisheng on 16/4/27.
+//  Copyright (c) 2016年 shisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
