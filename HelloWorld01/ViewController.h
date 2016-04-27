161553afef47ed1095b5e18b0d082ed063a4568c
git <git@qq.com>
2016-04-27 13:55:07 +0800

    ViewController add a click event.

--- HelloWorld01/ViewController.h
+++ HelloWorld01/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)versionControl:(id)sender;
 
 @end
 
--- HelloWorld01/ViewController.m
+++ HelloWorld01/ViewController.m
@@ -24,4 +24,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)versionControl:(id)sender {
+}
 @end
